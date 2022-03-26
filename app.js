806fde5e9cad9ca0dcb77537bac2ab2f23dd8c87
Adeoti Shuaib <Adeotishuaib2001@gmail.com>
2022-03-26 09:08:22 +0100

    Finished adding functionality to the page

--- app.js
+++ app.js
@@ -1,15 +1,25 @@
-const rate = document.getElementById("rate");
+const list = document.getElementById("rate");
 const submitBtn = document.querySelector(".submit-btn");
 const selectedRating = document.getElementById("selected-rating");
-
+const homeBtn = document.querySelector(".home");
 submitBtn.addEventListener("click", handleSubmit);
-rate.addEventListener("click", handleSelectRate);
+const thankYouState = document.querySelector(".thank-you-state");
+const selectedContainer = document.querySelector(".selected-container");
+list.addEventListener("click", handleSelectRate);
+homeBtn.addEventListener("click", backToHome);
 
 let ratingValue;
 function handleSelectRate(e) {
   ratingValue = e.target.textContent;
 }
 function handleSubmit(e) {
-  console.log("You clicked submit");
-  console.log(`You chose ${ratingValue}`);
+  if (ratingValue > 0) {
+    selectedRating.textContent = ratingValue;
+  } else {
+    selectedContainer.textContent = "You did not select a rating!";
+  }
+  thankYouState.classList.add("show");
+}
+function backToHome() {
+  thankYouState.classList.remove("show");
 }
